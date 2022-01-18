38aca859ab5d6642dded8f2dde5fabeb1eb29077
Muthukrishnan P <muthukrishnanpr@gmail.com>
2022-01-18 14:31:08 +0530

    v-3

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react'
 export default function App() {
   return (
     <div style={{display : "flex", justifyContent : "center", alignItems : "center", height : "100vh"}}>
-      <h2 style={{textTransform : "uppercase"}}>this is my first exercise to netlify app</h2>
+      <h2 style={{textTransform : "uppercase"}}>this is my first exercise to netlify application</h2>
     </div>
   )
 }
